49c60eadca27ea8c74e65566eb388496bb703a06
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-04-04 19:03:57 -0300

    build: set up app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const router = require("./routes/router");
+const cors = require("cors");
+
+const allowList = ["http://localhost:5173/"];
+const corsOptions = {
+  origin: allowList,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
